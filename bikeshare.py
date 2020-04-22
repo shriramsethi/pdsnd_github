6cec96fd851c46a8c68e2b1a1878801a3adb369c
Shriram Sethi <shriram.sethi54@gmail.com>
2020-04-22 21:51:31 +0530

    added initial documentation in get_filter() and load_data() functions in bikeshare.py file

---
+++ bikeshare.py
@@ -0,0 +1,82 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    
+
+    # return city
+    
+    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+            
+    
+def load_data(city, month, day):
+    """
+    Loads data for the specified city and filters by month and day if applicable.
+
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
+
+    # load data file into a dataframe
+    
+
+    # convert the Start Time column to datetime
+    
+    # extract month and day of week from Start Time to create new columns
+    
+    # filter by month if applicable
+        
+
+    # filter by month to create the new dataframe
+         
+    # filter by day of week if applicable
+    
+     # filter by day of week to create the new dataframe
+            
+
+    
+    # return df.fillna("Not Available", inplace = True)
+
+
+
+
+
+
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        display_data(df)
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
