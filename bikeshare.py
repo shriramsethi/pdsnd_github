502718bed3d6415b1100942f64a4ebfd7df39ef4
Shriram Sethi <shriram.sethi54@gmail.com>
2020-04-22 23:36:08 +0530

    imported pandas, time and numpy library and added main() function where program is going to start

---
+++ bikeshare.py
@@ -0,0 +1,27 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+        df = load_data(city, month, day)
+
+        time_stats(df)
+        station_stats(df)
+        trip_duration_stats(df)
+        user_stats(df)
+        display_data(df)
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
+
+
+if __name__ == "__main__":
+	main()
